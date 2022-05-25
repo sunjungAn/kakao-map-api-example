d1dab1e4a9352bd4dc95d7d237f6cd0236585703
SunjungAn <55094745+sunjungAn@users.noreply.github.com>
2022-05-25 18:12:27 +0900

    Update KakaoMapScript.js

--- kakao-map/src/Pages/KakaoMapScript.js
+++ kakao-map/src/Pages/KakaoMapScript.js
@@ -1,8 +1,8 @@
 import geojson from '../lib/GeoJSON.json';
-
+import '../test.css';
 const { kakao } = window;
 
-export default function KakaoMapScript() {
+export default function KakaoMapScript1() {
     let data = geojson.features;
     let coordinates = []; //좌표 저장 배열
     let name = ''; //행정구 이름
@@ -14,28 +14,29 @@ export default function KakaoMapScript() {
         center: new kakao.maps.LatLng(37.566826, 126.9786567),
         level: 9
     };
-    const map = new kakao.maps.Map(container, options);
-    const customOverlay = new kakao.maps.CustomOverlay({});
+    const map = new kakao.maps.Map(container, options); 
+
+    const customOverlay = new kakao.maps.CustomOverlay({
+    });
     const infowindow = new kakao.maps.InfoWindow({ removable: true });
-
-
+    
+    
     const displayArea = (coordinates, name) => {
-        let path = [];
+        let paths = [];
         let points = [];
-
       
         coordinates[0].forEach((coordinate) => {
           let point = {};
           point.x = coordinate[1];
           point.y = coordinate[0];
           points.push(point);
-          path.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
+          paths.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
         });
       
-        let polygon = new kakao.maps.Polygon({
+        const polygon = new kakao.maps.Polygon({
           map: map,
-          path: path, // 그려질 다각형의 좌표 배열입니다
-          strokeWeight: 2, // 선의 두께입니다
+          path: paths, // 그려질 다각형의 좌표 배열입니다
+          strokeWeight: 1, // 선의 두께입니다
           strokeColor: '#004c80', // 선의 색깔입니다
           strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
           strokeStyle: 'solid', // 선의 스타일입니다
@@ -44,22 +45,21 @@ export default function KakaoMapScript() {
         });
       
         polygons.push(polygon);
-      
+
         // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경합니다
         // 지역명을 표시하는 커스텀오버레이를 지도위에 표시합니다
         kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
           polygon.setOptions({ fillColor: '#09f' });
-      
+          console.log(points[0].x);
           customOverlay.setContent('<div class="area">' + name + '</div>');
-      
-          customOverlay.setPosition(mouseEvent.latLng);
+          customOverlay.setPosition(new kakao.maps.LatLng(points[0].x, points[1].y));
           customOverlay.setMap(map);
         });
       
         // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다
-        kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
-          customOverlay.setPosition(mouseEvent.latLng);
-        });
+         //kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
+          //customOverlay.setPosition(mouseEvent.latLng);
+         //});
       
         // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래색으로 변경합니다
         // 커스텀 오버레이를 지도에서 제거합니다
@@ -67,8 +67,6 @@ export default function KakaoMapScript() {
           polygon.setOptions({ fillColor: '#fff' });
           customOverlay.setMap(null);
         });
-      
-
     };
     
     data.forEach((val) => {
@@ -77,4 +75,5 @@ export default function KakaoMapScript() {
     
     displayArea(coordinates, name);
     });
-}
+  }
+
